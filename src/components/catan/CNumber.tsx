ccd923db369e903d1f484696c61b8e8edd51e256
Emerson Coronel <e.coronel@icloud.com>
2024-07-03 13:16:41 -0500

    catan board first iteration

---
+++ src/components/catan/CNumber.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface NumberProps {
+  value: number;
+}
+
+const CNumber: React.FC<NumberProps> = ({ value }) => {
+  const getImageForNumber = (num: number): string => {
+    const imageMap: { [key: number]: string } = {
+      2: '/images/catan/numbers/2.png',
+      3: '/images/catan/numbers/3.png',
+      4: '/images/catan/numbers/4.png',
+      5: '/images/catan/numbers/5.png',
+      6: '/images/catan/numbers/6.png',
+      8: '/images/catan/numbers/8.png',
+      9: '/images/catan/numbers/9.png',
+      10: '/images/catan/numbers/10.png',
+      11: '/images/catan/numbers/11.png',
+      12: '/images/catan/numbers/12.png',
+    };
+    return imageMap[num] || '/images/catan/numbers/12.png';
+  };
+
+  if (value === 0) {
+    return null;
+  }
+
+  return (
+    <div>
+      <img src={getImageForNumber(value)} alt={`Number ${value}`} style={{ width: '100%', height: '100%' }} />
+    </div>
+  );
+};
+
+export default CNumber;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,14 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
 
 const Home = () => {
-  const videoRef = useRef(null); // Step 2: Create a ref
-
-  useEffect(() => { // Step 3: Adjust playbackRate after component mounts
-    if (videoRef.current) {
-      videoRef.current.playbackRate = 0.5; // Half the normal speed
-    }
-  }, []);
-
   return (
     <>
       <Header />
       <div className="container-fluid">
-        <div id="home-text-container" className="text-center">
+        <div className="centered-container text-center">
           <p id="home-name">EMERSON CORONEL</p>
           <p id="home-subtext">SOFTWARE DEVELOPER</p>
         </div>
