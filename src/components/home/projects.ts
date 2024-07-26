02c599d5a6d9c443272ce6b1322850d5da6f0962
Emerson Coronel <dev@emersoncoronel.com>
2024-07-26 12:22:18 -0500

    EtzAi project card:

--- src/components/home/projects.ts
+++ src/components/home/projects.ts
@@ -1,13 +1,18 @@
 const projects = [
   {
+    title: "Typographia Typing Challenge",
+    imageUrl: "/images/project-cards/typographia.png",
+    link: "/typographia",
+  },
+  {
     title: "Settlers of Catan Board Generator",
     imageUrl: "/images/project-cards/catan-board.png",
     link: "/catan",
   },
   {
-    title: "Typographia Typing Challenge",
-    imageUrl: "/images/project-cards/typographia.png",
-    link: "/typographia",
+    title: "EtzAi Virtual Assistant",
+    imageUrl: "/images/project-cards/etzai.png",
+    link: "/etzai",
   },
 ];
 
