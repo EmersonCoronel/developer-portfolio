ea01483d000dd39998be99014603c02e96c29247
Emerson Coronel <dev@emersoncoronel.com>
2024-10-15 08:55:32 -0500

    removed script for now:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,25 +11,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Emerson Coronel - Software Developer</title>
-        <script type="application/ld+json">
-          {`
-            {
-              "@context": "https://schema.org",
-              "@type": "Person",
-              "name": "Emerson Coronel",
-              "url": "https://www.emersoncoronel.com",
-              "sameAs": [
-                "https://www.linkedin.com/in/emersoncoronel",
-                "https://github.com/EmersonCoronel"
-              ],
-              "jobTitle": "Software Engineer",
-              "worksFor": {
-                "@type": "Organization",
-                "name": "Genjo.ai"
-              }
-            }
-          `}
-        </script>
       </Head>
       <ParticlesComponent id="particles" />
       <Component {...pageProps} />
