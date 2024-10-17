00cf6fbcee26a6caa45aa29e81b52eafcd8b2ca3
Emerson Coronel <dev@emersoncoronel.com>
2024-10-17 17:06:08 -0500

    updated catan board generator to be completely self-encompassed

--- src/pages/catan.tsx
+++ src/pages/catan.tsx
@@ -1,44 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../components/general/Header";
-import { Board } from "catan-board-generator";
+import { CatanPage } from "catan-board-generator";
 
 const Catan: React.FC = () => {
-  const [expansion, setExpansion] = useState(false);
-  const [key, setKey] = useState(0);
-
-  const generateRegularBoard = () => {
-    setExpansion(false);
-    setKey((prevKey) => prevKey + 1);
-  };
-
-  const generateExpansionBoard = () => {
-    setExpansion(true);
-    setKey((prevKey) => prevKey + 1);
-  };
-
   return (
-    <div>
+    <>
       <Header />
-      <Board key={key} expansion={expansion} />
-      <div
-        className="button-container"
-        style={{ display: "flex", justifyContent: "center", margin: "20px" }}
-      >
-        <button
-          onClick={generateRegularBoard}
-          className="generate-board-button"
-        >
-          Generate Regular Board
-        </button>
-        <button
-          onClick={generateExpansionBoard}
-          className="generate-board-button"
-        >
-          Generate Expansion Board
-        </button>
-      </div>
-    </div>
+      <CatanPage />
+    </>
   );
 };
 
-export default Catan;
+export default Catan
