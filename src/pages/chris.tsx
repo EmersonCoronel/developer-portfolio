18b05ed1bcf19bd7ad3f419f7cfc6ab11079572f
Emerson Coronel <dev@emersoncoronel.com>
2024-12-14 14:23:20 -0800

    added chris

---
+++ src/pages/chris.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/general/Header";
+
+const Catan: React.FC = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Catan;
