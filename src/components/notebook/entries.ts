e586a8b6154cff8e9756b2832b977e82bc25e82c
Emerson Coronel <dev@emersoncoronel.com>
2025-04-06 21:12:11 -0500

    remove notebook

--- src/components/notebook/entries.ts
+++
@@ -1,9 +0,0 @@
-const entries = [
-  {
-    title: "Peru 2024",
-    imageUrl: "https://assets.emersoncoronel.com/images/notebook/peru2024/inca-7.webp",
-    link: "/notebook/peru2024",
-  },
-];
-
-export default entries;
