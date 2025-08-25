bc450a50857318183c800f9a72ff1e2945a0b2d5
Emerson Coronel <dev@emersoncoronel.com>
2025-08-25 15:34:02 -0500

    updated project cards

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -9,13 +9,13 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ title, imageUrl, link }) => {
   return (
-    <div className="w-[275px] m-auto text-primary border border-primary cursor-pointer bg-transparent h-[375px] flex flex-col">
+    <div className="w-[275px] m-auto text-primary border-2 border-primary cursor-pointer bg-transparent h-[375px] flex flex-col rounded-lg">
       <Link href={link} className="no-underline hover:no-underline">
-        <div className="h-[275px] overflow-hidden">
+        <div className="h-[275px] overflow-hidden rounded-t-lg">
           <img src={imageUrl} className="w-full h-full object-cover" alt={`${title} display image`} />
         </div>
-        <div className="p-4">
-          <h3 className="text-center text-xl whitespace-pre-wrap">{title}</h3>
+        <div className="p-4 rounded-b-lg">
+          <h3 className="text-center text-xl whitespace-pre-wrap font-medium">{title}</h3>
         </div>
       </Link>
     </div>
